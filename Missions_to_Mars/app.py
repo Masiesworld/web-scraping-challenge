8c3293d6c6f120a3fd8e68d33d2c2494ca1d2187
Masiesworld <50465138+Masiesworld@users.noreply.github.com>
2020-12-02 23:19:34 -0500

    Add files via upload

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_info"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars_info = mongo.db.mars_info.find_one()
+    return render_template("index.html", mars_info=mars_info)
+
+@app.route("/scrape")
+def scraper():
+    mars_info = mongo.db.mars_info
+    mars_data = scrape_mars.scrape()
+    mars_info.update({}, mars_data, upsert=True)
+    print("Done!")
+    print(mars_data)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
